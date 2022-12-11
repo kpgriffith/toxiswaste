4ea804ff62203bdc4e2979bf8088a077225cc051
Kevin Griffith <kpgriffith@me.com>
2022-12-11 07:01:50 -0600

    first commit

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"html/template"
+	"log"
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.ParseGlob("templates/*"))
+}
+
+func main() {
+
+	mux := httprouter.New()
+	mux.GET("/", index)
+	http.ListenAndServe(":8080", mux)
+
+}
+
+func index(w http.ResponseWriter, req *http.Request, _ httprouter.Params) {
+	err := tpl.ExecuteTemplate(w, "index.gohtml", nil)
+	HandleError(w, err)
+}
+
+func HandleError(w http.ResponseWriter, err error) {
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		log.Fatalln(err)
+	}
+}
